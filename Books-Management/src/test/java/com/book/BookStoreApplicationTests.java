37c356c5819c773d1047045afe2dd72f97d08f6a
2160731 <2160731@LTIN444067.cts.com>
2023-06-30 11:51:35 +0530

    latest and updated

--- Books-Management/src/test/java/com/book/BookStoreApplicationTests.java
+++ Books-Management/src/test/java/com/book/BookStoreApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hospital;
+package com.book;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HospitalManagementSystemApplicationTests {
+class BookStoreApplicationTests {
 
 	@Test
 	void contextLoads() {
